7301ca894605c3111cf9b4309a0835f1555c321d
gmgunter23 <gmgunter23@students.bmchs.org>
2022-03-09 09:18:04 -0600

    Worked on it

--- create_project.py
+++ create_project.py
@@ -1,5 +1,12 @@
 #### connect four
 
+from secrets import choice
+
+
+user1 = input("Hello Player 1, welcome to Connect Four! What is your name? > ")
+user2 = input("Hello Player 2, welcome to Connect Four! What is your name? > ")
+game = 'running'
+
 
 board = [['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-']]
 
@@ -15,6 +22,20 @@ def print_board(board):
     print(f"| {board[4][0]} | {board[4][1]} | {board[4][2]} | {board[4][3]} | {board[4][4]} | {board[4][5]} | {board[4][6]} |")
     print("-----------------------------")
     print(f"| {board[5][0]} | {board[5][1]} | {board[5][2]} | {board[5][3]} | {board[5][4]} | {board[5][5]} | {board[5][6]} |")
-    print("-----------------------------")
 
+print("Here is the board:")
 print_board(board)
+count = 0
+
+while game == 'running':
+    if count % 2 == 0.0:
+        choice_1_column = int(input("What column would you like to place the X?"))
+        board[choice_1_column] = 'X'
+        count = count + 1
+        print_board(board)
+    if count % 2 == 1.0:
+        choice_2_column = int(input("What column would you like to place the O?"))
+        board[choice_2_column] = 'O'
+        count = count + 1
+        print_board(board)
+
