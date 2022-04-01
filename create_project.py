57a6b102ebc5a87d8cd71d29373389895507994b
gmgunter23 <gmgunter23@students.bmchs.org>
2022-04-01 08:49:22 -0500

    Here you go Coach Gilpin

--- create_project.py
+++ create_project.py
@@ -1,9 +1,12 @@
 #### connect four
 
+from secrets import choice
+from urllib.parse import _ParseResultBase
+
+
 user1 = input("Hello Player 1, welcome to Connect Four! What is your name? > ")
 user2 = input("Hello Player 2, welcome to Connect Four! What is your name? > ")
 game = 'running'
-
 
 board = [['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-', '-']]
 
@@ -26,18 +29,64 @@ turn = 0
 
 while game == 'running':
     if turn % 2 == 0.0:
-        choice_1_column = int(input("What column would you like to place the X?"))
-        for i in board:
-            #### I have a problem here where I need to figure out how to do the gravity for connnect four ####
-            if board[i][choice_1_column] == '-':
-                board[i][choice_1_column] = 'X'
+        choice_1_column = input("What column would you like to place the X?")
+        run = 'run'
+        while run == 'run':
+            if isinstance(choice_1_column, int):
                 break
-            elif board[i][choice_1_column] == 'X' or board[i][choice_1_column] == 'O':
-                pass
+            else:
+                print("That is not a number, please try again.")
+                choice_1_column = int(input("What column would you like to place the X?"))
+                choice_1_column = choice_1_column - 1
+        choice_1_column = int(choice_1_column)
+        if choice_1_column > 7:
+            print("You cannot choose that number, that is too high. Please try again.")
+            choice_1_column = int(input("What column would you like to place the X?"))
+            choice_1_column = choice_1_column - 1
+        else:
+            choice_1_column = choice_1_column - 1
+        if board[0][choice_1_column] == 'X' or board[0][choice_1_column] == 'O':
+            print("That column is full, please try again.")
+            choice_1_column = int(input('What column would you like to place the X?'))
+            choice_1_column = choice_1_column - 1
+        if board[5][choice_1_column] == '-':
+            board[5][choice_1_column] = 'X'
+        elif board[4][choice_1_column] == '-':
+            board[4][choice_1_column] = 'X'   
+        elif board[3][choice_1_column] == '-':
+            board[3][choice_1_column] = 'X'
+        elif board[2][choice_1_column] == '-':
+            board[2][choice_1_column] = 'X'
+        elif board[1][choice_1_column] == '-':
+            board[1][choice_1_column] = 'X'
+        elif board[0][choice_1_column] == '-':
+            board[0][choice_1_column] = 'X'
+        elif board[0][choice_1_column] == 'X' or board[0][choice_1_column] == 'O':
+            print("That column is full, please try again.")
+            choice_1_column = int(input('What column would you like to place the X?'))
+            choice_1_column = choice_1_column - 1
+            
         turn = turn + 1
         print_board(board)
     if turn % 2 == 1.0:
         choice_2_column = int(input("What column would you like to place the O?"))
-        board[choice_2_column] = 'O'
+        if choice_2_column > 7:
+            print("You cannot choose that number, that is too high. Please try again.")
+            choice_2_column = int(input("What column would you like to place the O?"))
+            choice_2_column = choice_2_column - 1
+        else:
+            choice_2_column = choice_2_column - 1
+        if board[5][choice_2_column] == '-':
+            board[5][choice_2_column] = 'O'
+        elif board[4][choice_2_column] == '-':
+            board[4][choice_2_column] = 'O'   
+        elif board[3][choice_2_column] == '-':
+            board[3][choice_2_column] = 'O'
+        elif board[2][choice_2_column] == '-':
+            board[2][choice_2_column] = 'O'
+        elif board[1][choice_2_column] == '-':
+            board[1][choice_2_column] = 'O'
+        elif board[0][choice_2_column] == '-':
+            board[0][choice_2_column] = 'O'
         turn = turn + 1
         print_board(board)
